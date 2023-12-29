89826306124548bc83b2db191ed6c5a32ffdef99
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-29 13:54:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/dhanush2bot/Alpha-url-shortner-bot-free
-cd /Alpha-Url-Shortner-Bot
+cd /Alpha-url-shortner-bot-free
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
